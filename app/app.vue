da3f6beb866916289b63faec9c5ae1ccfaaf03e4
Sergey Kolesnikov <trm@mail.ru>
2025-12-03 15:15:26 +0300

    master - document.documentElement.style.setProperty version

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+const primaryColor = '#1e40af';
+const secondaryColor = '#9333ea';
+
+// Устанавливаем CSS переменные
+if (import.meta.client) {
+  document.documentElement.style.setProperty('--color-primary', primaryColor);
+  document.documentElement.style.setProperty('--color-secondary', secondaryColor);
+}
+</script>
+
 <template>
   <div>
     <AppHeader />
--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const h1Color = '#ff0000';
+const h2Color = '#00ff00';
+const h3Color = '#0000ff';
+</script>
 
 <template>
-  <h1 class="text-3xl font-bold underline"> Hello world! </h1>
+  <h1 class="text-3xl font-bold underline text-primary"> Hello world! </h1>
 </template>
 
 <style scoped></style>
