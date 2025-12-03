6d500a6f6a3343e15a28b53870d3e791d3171260
Sergey Kolesnikov <trm@mail.ru>
2025-12-03 15:05:01 +0300

    master - new tailwind dependency + AppHeader

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  // devServer: {
+  //   port: 3001
+  // }
+
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
