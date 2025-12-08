d320d18756b2a75de3ed5753b99cae923d376082
Sergey Kolesnikov <trm@mail.ru>
2025-12-09 01:25:02 +0300

    master - add icon & buttons for Banners

--- app/components/AppBanner.vue
+++ app/components/AppBanner.vue
@@ -20,10 +20,7 @@ const startStyle = computed(() => {
   };
 });
 
-const changeImage = () => {
-  bannerNumber.value =
-    (bannerNumber.value + 1) % props.banners.length;
-};
+let timer: number | null = null;
 
 watch(
   () => props.banners,
@@ -36,16 +33,45 @@ onMounted(() => {
   start();
 });
 
+function nextImage() {
+  bannerNumber.value =
+    (bannerNumber.value + 1) % props.banners.length;
+};
+
+function prevImage() {
+  bannerNumber.value =
+    (bannerNumber.value - 1 + props.banners.length) % props.banners.length;
+};
+
 function start() {
   if (props.banners.length > 1) {
-    setInterval(changeImage, props.delayInMs ?? 5000);
+    timer = setInterval(nextImage, props.delayInMs ?? 5000);
   }
+}
+
+function stop() {
+  if (timer) {
+    clearInterval(timer);
+    timer = null;
+  }
+}
+
+function next() {
+  stop();
+  nextImage();
+  start();
+}
+
+function prev() {
+  stop();
+  prevImage();
+  start();
 }
 </script>
 
 <template>
   <section
-    class="w-full p-0 h-96 bg-cover bg-center"
+    class="relative w-full p-0 h-96 bg-cover bg-center"
     :style="startStyle"
   >
     <div
@@ -54,6 +80,28 @@ function start() {
     >
       <h1 class="text-4xl font-bold mb-4">{{ currentBanner?.title }}</h1>
       <p class="text-lg mb-6">{{ currentBanner?.subtitle }}</p>
+    </div>
+
+    <div
+      v-if="props.banners.length > 1"
+      class="pointer-events-none absolute inset-0 flex items-center justify-between px-4"
+    >
+      <button
+        type="button"
+        class="pointer-events-auto flex h-12 w-12 items-center justify-center rounded-full bg-black/50 text-white shadow-lg transition hover:bg-black/70 focus:outline-none focus:ring-2 focus:ring-white/80"
+        aria-label="Previous slide"
+        @click="prev"
+      >
+        <Icon name="ph:caret-left-bold" size="24" />
+      </button>
+      <button
+        type="button"
+        class="pointer-events-auto flex h-12 w-12 items-center justify-center rounded-full bg-black/50 text-white shadow-lg transition hover:bg-black/70 focus:outline-none focus:ring-2 focus:ring-white/80"
+        aria-label="Next slide"
+        @click="next"
+      >
+        <Icon name="ph:caret-right-bold" size="24" />
+      </button>
     </div>
   </section>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,7 @@ export default defineNuxtConfig({
 
   modules: [
     '@pinia/nuxt',
+    '@nuxt/icon',
   ],
 
   css: [
